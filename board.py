d1432b969b73f248ce39fb3683e3bf53b2d28ca3
tictacno <37315015+tictacno@users.noreply.github.com>
2018-05-12 23:36:34 -0400

    Update board.py

--- board.py
+++ board.py
@@ -9,6 +9,12 @@ mib = [] #MIcro Board
 # [3][4][5]
 # [6][7][8]
 
+#Previous micro position (current macro)
+previous_y = -1 # -1 means any macro
+
+#0 (running), 1 (player 1 win), -1 (player 2 win), or 2 (draw)
+state = 0
+
 #Gets the player at a given location
 def get_pos(pos):
     return mib[pos[0]][pos[1]]
@@ -17,10 +23,10 @@ def get_pos(pos):
 def set_pos(player, pos):
     x = pos[0]
     y = pos[1]
-    if (x + y) > 0 and x < 9 and y < 9:
+    if  x > -1 and y > -1 and x < 9 and y < 9:
         if rules.valid_move(x, y):
             mib[x][y] = player
-            rules.update_board(x, player)
+            rules.check_micro(x, player)
             return True
         else:
             return False
@@ -29,6 +35,7 @@ def set_pos(player, pos):
         return False
 
 #Prints the board's current state
+#Needs fixing
 def printb():
     lines = []
     for i in range(0, 9):
@@ -36,13 +43,36 @@ def printb():
     for mi, micro in enumerate(mib):
         for ti, tile in enumerate(micro):
             if ti < 3:
-                lines[(3*math.floor(mi/3))] += str(tile)
+                lines[(3*math.floor(mi/3))] += str(tile) + ' '
             elif ti < 6:
-                lines[(3*math.floor(mi/3))+1] += str(tile)
+                lines[(3*math.floor(mi/3))+1] += str(tile) + ' '
             else:
-                lines[(3*math.floor(mi/3))+2] += str(tile)
-    for line in lines:
+                lines[(3*math.floor(mi/3))+2] += str(tile) + ' '
+    for line in translate(lines):
         print(line)
+
+#Translates board to readable list
+def translate(lines):
+    res = []
+    for l, line in enumerate(lines):
+        line_parts = line.split(' ')
+        new_line = ''
+        if (l % 3) == 0 and l != 0:
+            res.append(new_line)
+        for i, s in enumerate(line_parts):
+            if s == '0':
+                new_line += '_'
+            if s == '1':
+                new_line += 'X'
+            if s == '-1':
+                new_line += 'O'
+            if s == '2':
+                new_line += 'T'
+            if (i % 3) == 2:
+                new_line += '  '
+        res.append(new_line)
+    res.append('')
+    return res
 
 #Initializes board positions
 def init():
